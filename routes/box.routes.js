cad58d3b1b3bfc4b15a243477f6c5a1a7680667b
luthiwd <annattar@gmail.com>
2022-06-04 19:11:08 +0200

    Cambios en routas de box, incluimos populate

--- routes/box.routes.js
+++ routes/box.routes.js
@@ -1,67 +1,67 @@
-const BoxModel = require("../models/Box.model");
-
 const router = require("express").Router();
+const BoxModel = require("../models/Box.model");
+const FoodModel = require ('../models/Food.model')
 
 // POST  '/api/cajas/create' -> Creamos nueva caja
-router.post('/create', async (req, res, next) => {
-  // const { _id } = req.payload
-  const { name, boxmodel, foods, price } = req.body
-  const farmer = req.payload._id
+router.post("/create", async (req, res, next) => {
+  const { name, boxmodel, foods, price } = req.body;
 
   try {
     const createBox = await BoxModel.create({
-        name,
-        boxmodel,
-        foods,
-        price,
-        farmer
-    })
+      name,
+      boxmodel,
+      foods,
+      price,
+      farmer: req.payload._id,
+    });
 
-    res.json(createBox)
-
-  } catch(error) {
-    next(error)
+    res.json(createBox);
+  } catch (error) {
+    next(error);
   }
-})
+});
 
-// GET ('/api/cajas/:id) -> Mostramos caja
+// GET ('/api/cajas/:id/details) -> Mostramos caja
 router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
 
-    const { id } = req.params
-
-    try{ 
-        const response = await BoxModel.findById(id)
-        req.json(response)
-    } catch(error) {
-        next(error)
-    }
-})
+  try {
+    const response = await BoxModel.findById(id).populate("foods");
+    res.json(response);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // PATCH ‘/api/cajas/:id/edit’ -> Editamos Caja
-router.patch('/:id', async (req, res, next) => {
-  const { id } = req.params
-  const { name, boxmodel, foods, price } = req.body
+router.patch("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const { name, boxmodel, foods, price } = req.body;
 
   try {
+    const allFoods = await FoodModel.find()
     await BoxModel.findByIdAndUpdate(id, {
-            name,
-            boxmodel,
-            foods,
-            price
-    })
-      res.json("La caja se actualizó correctamente")
-  } catch(error) { next(error) }
-})
+      name,
+      boxmodel,
+      foods,
+      price,
+    });
+    res.status(200).json(allFoods);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // DELETE '/api/caja/:id/delete -> Borramos caja
-router.delete('/:id', async (req, res, next) => {
-
-  const { id } = req.params
+router.delete("/:id", async (req, res, next) => {
+  const { id } = req.params;
 
   try {
-    await BoxModel.findOneAndDelete(id)
-    res.json("La caja ha sido borrada")
-  } catch(error) { next(error) }
-})
+    await BoxModel.findOneAndDelete(id);
+    res.json("La caja ha sido borrada");
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
