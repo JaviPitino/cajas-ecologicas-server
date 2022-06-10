689a0ffdcd72f1a4581cddcbda86609ebe311814
luthiwd <annattar@gmail.com>
2022-06-10 16:23:46 +0200

    Ultimo push despues de presentación

--- routes/uploader.routes.js
+++ routes/uploader.routes.js
@@ -4,10 +4,7 @@ const uploader = require("../middlewares/uploader.js")
 
 // Ruta para enviar imagen a cloudinary y recibe un URL
 router.post("/", uploader.single("image"), (req, res, next) => {
-
-    console.log(req.file.path)
     res.json(req.file.path)
-
 })
 
 module.exports = router;
